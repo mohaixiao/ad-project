ef5478c31195a69be73a885f47959a09d2495c6d
mohaixiao <396367702@qq.com>
2022-08-02 16:33:44 +0800

    首次提交

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './style.scss'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='box'>
+        Hello,React
+        <span>I am jack</span>
+      </div>
+    )
+  }
+}
