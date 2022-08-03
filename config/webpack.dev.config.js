9a69f7290d8fc406907ca3073335b883eef1c491
mohaixiao <396367702@qq.com>
2022-08-03 11:47:14 +0800

    本地mock

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -4,6 +4,7 @@ const { merge } = require('webpack-merge');
 const OpenBrowserPlugin = require('open-browser-webpack4-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpackConfigBase = require('./webpack.base.config');
+const mockMiddleware = require('./mock.config');
 
 const PORT = 8080;
 const webpackConfigDev = {
@@ -21,7 +22,18 @@ const webpackConfigDev = {
         }),
     ],
     devtool: 'eval-source-map',
-
+    devServer: {
+        contentBase: path.join(__dirname, '../src'),
+        historyApiFallback: false,
+        hot: false,
+        host: '0.0.0.0',
+        port: PORT,
+        before(app) {
+            const projectDir = path.resolve();
+            const mockDir = './mock';
+            app.use(mockMiddleware({ projectDir, mockDir }));
+        },
+    },
 };
 
 // module.exports = smp.wrap(merge(webpackConfigBase, webpackConfigDev));
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,11 +1,29 @@
 import React, { Component } from "react";
 interface Props {}
-import {Button} from 'antd';
+import "./style.scss";
+import { Button } from "antd";
+import axios from "axios";
 
 export default class IndexPage extends Component<Props> {
+  componentDidMount() {
+    axios
+      .get('/ad/index/gray')
+      .then((res) => {
+        console.log("res", res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   render() {
-    return <div>IndexPage
-      <Button type="primary">按钮</Button>
-    </div>;
+    return (
+      <div className="index-page">
+        <div className="middle-box">
+          IndexPage
+          <Button type="primary">按钮</Button>
+        </div>
+      </div>
+    );
   }
 }
